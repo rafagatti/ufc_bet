6d648a831a08d8ea1e7f8216d29b1af71942c2ec
Rafael de Oliveira Gatti <rafaelgatti3@gmail.com>
2020-11-10 10:45:37 -0300

    Incluindo leitura provisoria dos dados a partir do diretorio data/

--- code/exploratorio.py
+++ code/exploratorio.py
@@ -1,10 +1,13 @@
-import os
 import pandas as pd
-from decorador import gerador_try_except
 
 
-df_recents_events = pd.read_csv("C://Users/RafaeldeOliveiraGatt/Desktop/ufc/data/most-recent-event.csv")
-df_upcoming_events = pd.read_csv("C://Users/RafaeldeOliveiraGatt/Desktop/ufc/data/upcoming-event.csv")
-df_ufc_master = pd.read_csv("C://Users/RafaeldeOliveiraGatt/Desktop/ufc/data/ufc-master.csv")
-df_task_dummy = pd.read_csv("C://Users/RafaeldeOliveiraGatt/Desktop/ufc/data/task-dummy.csv")
+if __name__ == "__main__":
+    
+    path = 'C://Users/RafaeldeOliveiraGatt/Desktop/ufc/data'
 
+    most_recent_event = pd.read_csv('{}/{}.{}'.format(path, 'most-recent-event', 'csv'))
+    task_dummy = pd.read_csv('{}/{}.{}'.format(path, 'task-dummy', 'csv'))
+    ufc_master = pd.read_csv('{}/{}.{}'.format(path, 'ufc-master', 'csv'))
+    upcoming_events = pd.read_csv('{}/{}.{}'.format(path, 'upcoming-event', 'csv'))
+
+    
